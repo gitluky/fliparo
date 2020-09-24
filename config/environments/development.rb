c8619f5d6dc8942ff39f56efdd3c7e87292bb842
gitluky <jluk87@gmail.com>
2020-09-24 12:42:26 -0400

    Installed devise, rack-cors and dotenv-rails. Set up CORS configuration in application.rb.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,45 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+#  ===============================================================================
+
+# Depending on your application's configuration some manual setup may be required:
+
+#   1. Ensure you have defined default url options in your environments files. Here
+#      is an example of default_url_options appropriate for a development environment
+#      in config/environments/development.rb:
+
+#        config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+#      In production, :host should be set to the actual host of your application.
+
+#      * Required for all applications. *
+
+#   2. Ensure you have defined root_url to *something* in your config/routes.rb.
+#      For example:
+
+#        root to: "home#index"
+     
+#      * Not required for API-only Applications *
+
+#   3. Ensure you have flash messages in app/views/layouts/application.html.erb.
+#      For example:
+
+#        <p class="notice"><%= notice %></p>
+#        <p class="alert"><%= alert %></p>
+
+#      * Not required for API-only Applications *
+
+#   4. You can copy Devise views (for customization) to your app by running:
+
+#        rails g devise:views
+       
+#      * Not required *
+
+# ===============================================================================
+  # Default URL Options - part of Devise installation - In production, :host should be set to the actual host of your application.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
